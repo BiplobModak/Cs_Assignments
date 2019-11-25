315927c9b3bfb90f697b46ea76e739fa7133b1cf
BiplobKumarModak <biplob01.modak@gmail.com>
2019-11-25 21:40:54 +0600

    c# flowcontrol projects

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            /*
+                frist piramid 
+                      *
+                     * *
+                    * * * 
+
+            */
+            for(i = 0; i<10 ; i++){
+                for(j =10; j>i; j--){
+                    Console.Write(" ");
+                
+                }
+                for(j =0; j<i; j++){
+                    Console.Write("* ");
+                
+                }
+                Console.WriteLine("");   
+
+            }
+
+            /*
+                2nd piramid 
+                  * * *
+                   * *
+                    * 
+
+            */
+             for(i = 0; i<10 ; i++){
+                for(j =0; j<i; j++){
+                    Console.Write(" ");
+                
+                }
+                for(j =10; j>i; j--){
+                    Console.Write("* ");
+                
+                }                
+                Console.WriteLine("");   
+
+            }
+             /*
+                3nd piramid 
+                  *
+                  * *
+                  * * *
+                  * *
+                  * 
+
+            */
+
+            for(i = 0; i<10 ; i++){
+                for(j =0; j<i; j++){
+                    Console.Write("* ");
+                
+                }
+                for(j =10; j>i; j--){
+                    Console.Write(" ");
+                
+                }                
+                Console.WriteLine("");   
+
+            }
+            for(i = 0; i<10 ; i++){
+                
+                for(j =10; j>i; j--){
+                    Console.Write("* ");
+                
+                }                
+                Console.WriteLine("");   
+
+            }
+            /*
+                3nd piramid 
+                      *
+                    * *
+                  * * *
+                    * *
+                      * 
+
+            */
+            for(i = 0; i<10 ; i++){
+                for(j =10; j>i; j--){
+                    Console.Write(" ");
+                
+                }
+                for(j =0; j<=i; j++){
+                    Console.Write("*");
+                
+                }                                
+                Console.WriteLine("");   
+
+            }
+            for(i = 0; i<10 ; i++){
+                for(j =0; j<=i; j++){
+                    Console.Write(" ");
+                
+                }
+                for(j =10; j>i; j--){
+                    Console.Write("*");
+                
+                }
+                                                
+                Console.WriteLine("");   
+
+            }
+            
+
+        }
+    }
+}
